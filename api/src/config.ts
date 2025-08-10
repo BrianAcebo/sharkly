f40840be68ac51f2440181864714e2bf6849c180
Brian Acebo <brianacebo@gmail.com>
2025-08-10 17:29:07 -0400

    updated nodemailer config env handling

--- api/src/config.ts
+++ api/src/config.ts
@@ -12,12 +12,12 @@ interface Config {
 
 export const config: Config = {
 	smtp: {
-		host: process.env.SMTP_HOST || 'smtp.gmail.com',
-		port: parseInt(process.env.SMTP_PORT || '587'),
+		host: process.env.SMTP_HOST ? process.env.SMTP_HOST : 'smtp.gmail.com',
+		port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
 		secure: process.env.SMTP_SECURE === 'true',
-		user: process.env.SMTP_USER || '',
-		pass: process.env.SMTP_PASS || '',
-		from: process.env.SMTP_FROM || 'noreply@paperboatcrm.com'
+		user: process.env.SMTP_USER ? process.env.SMTP_USER : '',
+		pass: process.env.SMTP_PASS ? process.env.SMTP_PASS : '',
+		from: process.env.SMTP_FROM ? process.env.SMTP_FROM : 'noreply@paperboatcrm.com'
 	},
-	frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173'
+	frontendUrl: process.env.FRONTEND_URL ? process.env.FRONTEND_URL : 'http://localhost:5173'
 };
