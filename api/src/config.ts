b47ee38e714862095adf44932c86d5c638d28e77
Brian Acebo <brianacebo@gmail.com>
2025-08-10 17:51:04 -0400

    updated nodemailer config env handling

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,34 +1,31 @@
 interface Config {
 	smtp: {
-		host: string | undefined;
-		port: number | undefined;
+		host?: string;
+		port?: number;
 		secure: boolean;
-		user: string | undefined;
-		pass: string | undefined;
-		from: string | undefined;
+		user?: string;
+		pass?: string;
+		from?: string;
 	};
-	frontendUrl: string | undefined;
+	frontendUrl?: string;
 }
 
-// Access environment variables in a way Vercel can handle
-const env = {
-	SMTP_HOST: process.env.SMTP_HOST,
-	SMTP_PORT: process.env.SMTP_PORT,
-	SMTP_SECURE: process.env.SMTP_SECURE,
-	SMTP_USER: process.env.SMTP_USER,
-	SMTP_PASS: process.env.SMTP_PASS,
-	SMTP_FROM: process.env.SMTP_FROM,
-	FRONTEND_URL: process.env.FRONTEND_URL
-};
+const SMTP_HOST = process.env.SMTP_HOST ?? 'smtp.gmail.com';
+const SMTP_PORT = process.env.SMTP_PORT ? Number(process.env.SMTP_PORT) : 587;
+const SMTP_SECURE = process.env.SMTP_SECURE === 'true' || SMTP_PORT === 465;
+const SMTP_USER = process.env.SMTP_USER;
+const SMTP_PASS = process.env.SMTP_PASS;
+const SMTP_FROM = process.env.SMTP_FROM ?? SMTP_USER;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 export const config: Config = {
 	smtp: {
-		host: env.SMTP_HOST,
-		port: env.SMTP_PORT ? Number(env.SMTP_PORT) : undefined,
-		secure: env.SMTP_SECURE === 'true',
-		user: env.SMTP_USER,
-		pass: env.SMTP_PASS,
-		from: env.SMTP_FROM
+		host: SMTP_HOST,
+		port: SMTP_PORT,
+		secure: SMTP_SECURE,
+		user: SMTP_USER,
+		pass: SMTP_PASS,
+		from: SMTP_FROM
 	},
-	frontendUrl: env.FRONTEND_URL
+	frontendUrl: FRONTEND_URL
 };
